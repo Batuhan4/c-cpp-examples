07913a413fb0eaefc0d1c5ec65ecdc81ffb11638
Batuhan4 <53948634+Batuhan4@users.noreply.github.com>
2020-04-19 22:21:35 +0300

    Update binance_fee_calculator.c
    added command lines

--- binance_fee_calculator.c
+++ binance_fee_calculator.c
@@ -11,6 +11,11 @@ printf("%.2f is  commision",fee_calc(trade_vol)); // didnt assigned it to a valu
 
 /*
 sorry for this short code but idk :/
+1        1
+alıp satmadaki kar oranı 0.12% olsa bile zarar etmiyosun demek bu
+1000 dolarda 60 cent
+1 al 1 sat 1.20 dolar
+1.20/1000 dolar = 0.12/100
 */
 
 }
